968924ec695a315132cab614b98175b160a50052
domcabral <domcabral@protonmail.com>
2026-07-20 11:47:47 -0300

    feat(workflow): aciona a emissao do parecer tecnico em estados terminais
    WorkflowService.decideStep() chama TechnicalOpinionService quando a
    decisao fecha o fluxo: REJECT gera parecer Rejeitado imediatamente;
    APPROVE na ultima etapa elegivel gera parecer Homologado. Testes
    atualizados com o mock de TechnicalOpinionService e novas asserts
    confirmando a chamada com tenantId/assessmentId/status/issuedById
    corretos nos dois casos.

--- apps/api/src/modules/workflow/workflow.module.ts
+++ apps/api/src/modules/workflow/workflow.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { SeparationOfDutiesService } from "../../common/services/separation-of-duties.service";
+import { TechnicalOpinionModule } from "../technical-opinions/technical-opinion.module";
 import { WorkflowController } from "./workflow.controller";
 import { WorkflowRepository } from "./workflow.repository";
 import { WorkflowService } from "./workflow.service";
 
 @Module({
+  imports: [TechnicalOpinionModule],
   controllers: [WorkflowController],
   providers: [WorkflowRepository, WorkflowService, SeparationOfDutiesService],
   exports: [WorkflowService],
